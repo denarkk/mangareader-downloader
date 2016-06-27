a2e34756230806b467e00af03317fecdf85764ac
Luis <luishendrix92@gmail.com>
2016-06-26 19:59:20 -0700

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,27 @@
+
+# The manga provider to download the pages from
+PROVIDER = "http://www.mangareader.net/"
+
+# The folder where the episode is going to be saved
+LOCAL_PATH = "/home/luishendrix92/Imágenes/" # + seriesName/epNumber/
+
+# Determines from which page number to start looping
+INITIAL_PAGE = 1
+
+# Message displayed at the end of the dowload loop
+SUCCESS_MESSAGE = "The episode has been successfully downloaded!"
+
+# Message to be displayed while downloading a page
+DOWLOADING_MESSAGE = "Currently downloading page #" # + pageNumber
+
+# Message to be displayed when the episode isn't released yet
+NOT_RELEASED = "This episode hasn't been released yet... sorry, mate."
+
+# Message to be displayed for HTTP request errors
+DOESNT_EXIST = "This series doesn't exist in the database!"
+
+# Maximum amount of digits expected as the page number
+ESTIMATED_MAX_DIGITS = 3
+
+# The extension of the image file in the URL
+FILE_EXT = ".jpg"
